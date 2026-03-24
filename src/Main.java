68b289086cc6561fece4b85128d45a20d5dcb918
alkthg2-stack <alkthg2@gmail.com>
2026-03-24 15:57:32 +0100

    Mission 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public  class Main {
+    static void main() {
+
+        String Marque = "Peugeot";
+        int Modele = 208;
+        int annee = 2021;
+        int prix = 14500;
+
+        System.out.println("La voiture "+Marque+" "+Modele+" de l'année "+annee+" au prix de : "+prix+" euros.");
+
+    }
+}
